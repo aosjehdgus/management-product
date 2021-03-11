84efe0a6451a0f60997841e2ce5a6aafa38bbcc0
kdhsea <kdhsea@gmail.com>
2021-03-11 14:25:13 +0900

    Revise Customer.js Use map function

--- src/App.js
+++ src/App.js
@@ -2,32 +2,61 @@ import React, {Component} from 'react';
 import Customer from './components/Customer';
 import './App.css';
 
-const customer = {
-
+//customers를 배열 형태로 만든다(여러명의 고객을 추가하기 위해서)
+// 
+const customers = [
+  
+{
   'id'       : '1',
-  'image'    : 'https://placeimg.com/64/64/any',
+  'image'    : 'https://placeimg.com/64/64/1',
   'name'     : '동글',
   'birthday' : '930121',
   'gender'   : '남자',
   'job'      : '개발자'
-
+},
+{
+  'id'       : '2',
+  'image'    : 'https://placeimg.com/64/64/2',
+  'name'     : '당그리',
+  'birthday' : '930121',
+  'gender'   : '남자',
+  'job'      : '개발자'
+},
+{
+  'id'       : '3',
+  'image'    : 'https://placeimg.com/64/64/3',
+  'name'     : '치동',
+  'birthday' : '930121',
+  'gender'   : '남자',
+  'job'      : '개발자'
 }
+
+]
 
 
 class App extends Component{
   render(){
 
     return(
+        <div>
+          {
+            customers.map(c => {
+              return(
+                <Customer
+                  key      = {c.id} 
+                  id       = {c.id}
+                  image    = {c.image}
+                  name     = {c.name}
+                  birthday = {c.birthday}
+                  gender   = {c.gender}
+                  job      = {c.job}
+                />
+              );            
+            })
+          }
+           
+        </div>
 
-      <Customer 
-        id        = {customer.id}
-        image     = {customer.image}
-        name      = {customer.name}
-        birthday  = {customer.birthday}
-        gender    = {customer.gender}
-        job       = {customer.job}
-            
-      />
 
     );
 
