abd8e0e0497a538d46a649a98450b7e9b0132302
kdhsea <kdhsea@gmail.com>
2021-03-12 13:48:10 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React, {Component} from 'react';
 import Customer from './components/Customer';
 import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles} from '@material-ui/core/styles';
+
+const styles = theme => ({
+
+    root : {
+      width : '100%',
+      marginTop : theme.spacing.unit * 3,
+      overflowX : "auto"
+    },
+
+    table : {
+      minWidth : 1080
+    }
+
+
+})
+
 
 //customers를 배열 형태로 만든다(여러명의 고객을 추가하기 위해서)
-// 
+
 const customers = [
   
 {
@@ -37,25 +60,41 @@ const customers = [
 class App extends Component{
   render(){
 
+    const { classes } = this.props;
+
     return(
-        <div>
-          {
-            customers.map(c => {
-              return(
-                <Customer
-                  key      = {c.id} 
-                  id       = {c.id}
-                  image    = {c.image}
-                  name     = {c.name}
-                  birthday = {c.birthday}
-                  gender   = {c.gender}
-                  job      = {c.job}
-                />
-              );            
-            })
-          }
-           
-        </div>
+        <Paper className = {classes.root}>
+          <Table className = {classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {
+                customers.map(c => {
+                  return(
+                    <Customer
+                      key      = {c.id} 
+                      id       = {c.id}
+                      image    = {c.image}
+                      name     = {c.name}
+                      birthday = {c.birthday}
+                      gender   = {c.gender}
+                      job      = {c.job}
+                    />
+                  );            
+                })
+              }
+            </TableBody>
+         </Table>
+
+        </Paper>
 
 
     );
@@ -65,4 +104,4 @@ class App extends Component{
 }
 
 
-export default App;
+export default withStyles(styles)(App);
