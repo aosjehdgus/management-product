8d21775bef7c2ca4ffdd2e904d35a3739d54709e
kdhsea <kdhsea@gmail.com>
2021-03-17 00:13:17 +0900

    Create Customer Database Table & Node.js express Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const express    = require('express');
 const bodyParser = require('body-parser');
 const app  = express();
@@ -6,37 +8,36 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
 
-//customers api 만들기
+// database.json 파일 읽어오기
+// 해당 환경 설정 데이터 parsing해서 가져오기
+// mysql 불러오기
+
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+// 연결과 관련된 변수 설정하기
+
+const connection = mysql.createConnection({
+    host : conf.host,
+    user : conf.user,
+    password : conf.password,
+    port : conf.port,
+    database : conf.database
+});
+connection.connect();
+
+// customers api 만들기
+// 사용자가 customers 경로에 접속을 한 경우
+// DB에 접근해서 query를 날릴 수 있도록 코딩
 
 app.get('/api/customers', (req, res) => {
-
-    res.send([
-        {
-            'id'       : '1',
-            'image'    : 'https://placeimg.com/64/64/1',
-            'name'     : '동글',
-            'birthday' : '930121',
-            'gender'   : '남자',
-            'job'      : '프론트엔드 개발자'
-            },
-            {
-            'id'       : '2',
-            'image'    : 'https://placeimg.com/64/64/2',
-            'name'     : '당그리',
-            'birthday' : '950204',
-            'gender'   : '여자',
-            'job'      : '디자이너'
-            },
-            {
-            'id'       : '3',
-            'image'    : 'https://placeimg.com/64/64/3',
-            'name'     : '치동',
-            'birthday' : '911023',
-            'gender'   : '남자',
-            'job'      : '웹 퍼블리셔'
-            }
-    ]);
-
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    )
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
